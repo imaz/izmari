f32cf9f77c918f3c87d19bf0bcc272a4cfa78046
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-02-16 10:18:32 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Izimaz::Application.config.secret_token = 'b7e9088601c0faa184d9140c5c5c9da0ed9f71996685fb19990505e193119d9c82ff914aba8c1170e77fcafed7941f857928c2f752d4a8bce1a733d623b3bcb1'
